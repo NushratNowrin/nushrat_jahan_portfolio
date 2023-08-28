e7fb054f68be61362e4379661b42f1ab54c285fe
NushratNowrin <nowrin2023@gmail.com>
2023-08-29 00:44:44 +0600

    style added in skills

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Skill = (skill) => {
-    const {img, name} = skill
-    return (
-        <div>
-            <div className='  flex items-center  justify-center'><img className='h-16 w-16 bg-white rounded-xl' src={img} alt={name} /></div>
-        </div>
-    );
+	const { img, name } = skill;
+	return (
+		<div className="">
+			<div className='skill-container  flex items-center  justify-center'>
+				<img
+					className='h-16 w-16 bg-white border-2 border-white rounded-xl skill front relative'
+					src={img}
+					alt={name}
+				/>
+                {/* <p className='h-16 w-16 bg-white border-2 border-white rounded-xl skill back text-black text-xs p-2 absolute'>
+                    {name}
+                </p> */}
+			</div>
+		</div>
+	);
 };
 
 export default Skill;

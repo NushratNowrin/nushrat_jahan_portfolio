834f2c6e3816bc2efe5f524f3dcfc81fc3df2201
NushratNowrin <nowrin2023@gmail.com>
2023-08-29 12:37:19 +0600

    som modification added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsArrowUpRight } from "react-icons/bs";
+import { FiLink } from "react-icons/fi";
 
 const Project = (project) => {
 	const { name, img, description, technologies, livelink } = project;
@@ -11,9 +11,25 @@ const Project = (project) => {
 			<div
 				className={`md:w-1/2 h-80 project-img bg-[url('${img}')] bg-cover`}></div>
 			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/craftopia.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/Princess-Palette.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/deshi-delights.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/career-flock.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/Ema-john.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/knowledge-station.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/AI-universe.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/Geometry-genius.png')]`}></div> */}
+			{/* <div
+				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/Quiz-hero.png')]`}></div> */}
+			{/* <div
 				className={`md:w-1/2 h-80 project-img bg-[url('/Projects/bank-calculation.png')]`}></div> */}
-			
-			
 
 			<div className='relative md:w-1/2 md:h-64 sm:h-52 h-80 p-5 bg-secondary  project-cart'>
 				<h2 className='text-primary uppercase font-semibold text-xl tracking-widest mb-3'>
@@ -25,7 +41,7 @@ const Project = (project) => {
 				</p>
 				<div className='absolute -bottom-5 right-1/2 bg-secondary rounded-full w-10 h-10 z-30 arrow-box flex items-center justify-center'>
 					<a href={livelink} target='_blank' rel='noreferrer'>
-						<BsArrowUpRight />
+						<FiLink />
 					</a>
 				</div>
 			</div>
